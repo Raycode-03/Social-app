ba5306a3b05d9bf7821701b3d062b610be322a82
Akerele  Raymond <akereleolasun5@com>
2025-10-04 15:24:24 +0100

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+'use client'
 
-function page() {
+import React from 'react'
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
+function Page() {
+  const router = useRouter()
+  
+  useEffect(() => {
+    router.push("/auth/login")
+  }, [router])
+
   return (
-    <div>Home page</div>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="text-lg">Redirecting to login...</div>
+    </div>
   )
 }
 
-export default page
+export default Page
