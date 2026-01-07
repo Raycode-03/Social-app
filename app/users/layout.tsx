56b5c496d2d24fa57703d17336c9c9f1bae029c7
Raycode-03 <akereleolasun5@gmail.com>
2026-01-07 21:17:19 +0100

    fix

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,10 +1,10 @@
+
 import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar"
 import {auth } from "../api/auth/[...nextauth]/auth"
 import Navbar from "@/components/users/navbar"
 import { AppSidebar } from "@/components/users/sidebar"
 import {connect_db} from "@/lib/mongodb"
 import { redirect } from "next/navigation"
-
 // import Sidebarmenu from "@/components/users/sidebarmenu" 
 export default async function Layout({ children }: { children: React.ReactNode }) {
    const NAVBAR_HEIGHT = 84;
